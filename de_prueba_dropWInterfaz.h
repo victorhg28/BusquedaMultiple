e38a40dccc2d03b2f4642d53cdd16d48688d9ff0
Victor Paul Morante Vargas <C26681@TIM.COM.PE>
2024-02-15 13:18:34 -0500

    añadida funcionalidad para seleccionar color

--- de_prueba_dropWInterfaz.h
+++ de_prueba_dropWInterfaz.h
@@ -12,15 +12,32 @@ class de_prueba_dropWInterfaz : public Withde_prueba_dropWInterfazLayout<TopWind
 //class de_prueba_dropWInterfaz : public WithVer_si_existen_IPSLayout<TopWindow> {
 public:
 	
-	virtual void DragAndDrop(Point p, PasteClip& d);
-	virtual void LeftDrag(Point p, dword keyflags);
-	virtual void Paint(Draw &w);
+	virtual void DragAndDrop(Point p, PasteClip& d) override;
+	virtual void LeftDrag(Point p, dword keyflags) override;
+	virtual void Paint(Draw &w) override;
+	
+	//variable para gestionar archivos de drag & drop
 	Vector<String> files;
 	
+	//mis metodos
 	void buscar();
+	//void Slider();
+	void Slider();
 	
+	//mis variables
 	std::string archivoMML="";
+	//Color colorBG;
+	
+	Color cc;
+	//	virtual void Paint(Draw& w) override
+	//	{
+      //                  auto r = GetRect();
+		//	w.DrawRect(r, cc);
+          //      }
+	
+	
 
+	//constructor
 	de_prueba_dropWInterfaz();
 };
 
--- main.cpp
+++ main.cpp
@@ -11,6 +11,27 @@ de_prueba_dropWInterfaz::de_prueba_dropWInterfaz()
 	
 	CtrlLayout(*this, "Window title");
 	
+	//sliders color de fondo
+	bg_rojo.MinMax(0,255);
+	bg_verde.MinMax(0,255);
+	bg_azul.MinMax(0,255);
+	
+	
+	
+	
+	//bg_rojo << THISFN(Slider());
+	
+	bg_rojo << [=] { Slider(); };
+	
+	//Slider();
+	
+	//bg_rojo << THISFN(Slider);
+    //slider.Range(700);
+    //slider <<= 250;
+	
+	//this.DrawRect(GetSize(),Color(bg_rojo.GetData(),bg_verde.GetData(),bg_azul.GetData()));
+	
+	
 	btn_buscar << [=] { buscar(); };
 	
 	//ofstream archivo_resultado("script_generado.txt", std::ios::trunc);
@@ -27,6 +48,26 @@ de_prueba_dropWInterfaz::de_prueba_dropWInterfaz()
 	//label1.SetText("asdasd");
 }
 
+void de_prueba_dropWInterfaz::Slider(){
+	//dynarect.SetRectX(50, ~slider);
+	//text = "\1[C6*/@b " + AsString(~slider);
+	//Color c(~bg_rojo,0,0);
+	//colorBG = Color(~bg_rojo,0,0);
+	
+	static_text.SetText(AsString(~bg_rojo));
+	//static_text.SetText((String)bg_rojo.GetData());
+	//Upp:Value
+	
+	//SliderCtrl
+	
+	//static_text.SetText("asdas");
+	
+	//Color c;
+	//c = Color(0,0,0);
+	
+	//DrawRect(GetSize(),Color(bg_rojo.GetData(),bg_verde.GetData(),bg_azul.GetData()));
+	//return c;
+}
 
 void de_prueba_dropWInterfaz::DragAndDrop(Point p, PasteClip& d)
 {
@@ -48,6 +89,9 @@ void de_prueba_dropWInterfaz::LeftDrag(Point p, dword keyflags)
 }
 
 
+
+
+
 void de_prueba_dropWInterfaz::buscar(){
 	string str_aux_text="";
 		
@@ -63,29 +107,35 @@ void de_prueba_dropWInterfaz::buscar(){
 				archivo_resultado<<str_aux_text<<"\n";
 			}
 			
-			
-			//logica para encontrar texto exacto
-			//if (str_aux_text=="aaaa"){
-			//	archivo_resultado<<str_aux_text<<"\n";
-			//}
-		}
-		
-		
-		
-		//ofstream archivo_resultado(valor, std::ios::trunc);
-	
-		//archivo_resultado<< "// Filtros ////////////////////////////////////////////////////////";
-		//archivo_resultado<<valor;
-		
-		
-		//ifstream ifstream_archivo(files[0].ToStd());
 		
 		PromptOK("Resultado Generado");
+	}
 }
 
 
-void de_prueba_dropWInterfaz::Paint(Draw &w)
-{
+void de_prueba_dropWInterfaz::Paint(Draw &w) {
+	//String
+	//cc= color_rgb_Sliders(StrInt(AsString(~bg_rojo)),0,0);
+	
+	//auto r = GetRect();
+	//w.DrawRect(r, color_rgb_Sliders);
+	//LAYOUTFILE.changeColor(Color(0,0,0));
+	w.DrawRect(GetSize(),Color(StrInt(AsString(~bg_rojo)),0,0));//RGB
+	Refresh();
+	
+	
+	
+	//Slider();
+	
+	//Update();
+	
+	//w.DrawRect(GetSize(),Color(StrInt(AsString(~bg_rojo)),0,0));//RGB
+	//w.DrawRect(GetSize(),Color(29,21,34));//RGB
+	//w.DrawRect(GetSize(),Color(0,0,0));
+	//w.DrawRect(GetSize(),Color(bg_rojo.GetData(),bg_verde.GetData(),bg_azul.GetData()));//RGB
+	
+
+	
 	/*
 	w.DrawText(2, 2, "None");
 	if(files.GetCount()){
@@ -93,11 +143,6 @@ void de_prueba_dropWInterfaz::Paint(Draw &w)
 	}
 	*/
 	//label1.SetText("antes");
-	
-	
-	
-	
-	
 	
 	if(files.GetCount()){
 		//w.DrawText(2, 2 * Draw::GetStdFontCy(), files[0]);
